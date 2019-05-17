f4abc0cb096786c3ee4ea4aca669a4121d9a2820
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-17 15:24:20 +0200

    build and deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyB_yI9LkAV9SW2C6OYWykDVdeN9eEg3n-Y",
+    authDomain: "rang-13fc3.firebaseapp.com",
+    databaseURL: "https://rang-13fc3.firebaseio.com",
+    projectId: "rang-13fc3",
+    storageBucket: "rang-13fc3.appspot.com",
+    messagingSenderId: "1071191593287",
+    appId: "1:1071191593287:web:3cdd5498d452d659"
   }
 };
