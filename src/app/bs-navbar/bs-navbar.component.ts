8c5b964d4f557c531a3b076639fda5334cc1bc26
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-21 23:40:52 +0200

    Subscribing authState for showing the name of user and hide login

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-
+import *  as firebase from 'firebase';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
@@ -8,8 +8,10 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class BsNavbarComponent {
 
+  user: firebase.User;
+
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(x => console.log(x));
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
 
