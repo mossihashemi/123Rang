697c29f3754af06d55cfb5a98d2be9a232d8bcb8
mossiXYZ <mossi.hashemi@gmail.com>
2019-05-28 19:55:16 +0200

    293 Redirecting after Logging In

--- src/app/auth-gaurd.service.ts
+++ src/app/auth-gaurd.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthGaurdService implements CanActivate {
 
-  constructor(private auth: AuthService, private router: Router) { }
-  canActivate() {
+  constructor(
+    private auth: AuthService,
+    private router: Router) { }
+
+  /* 
+  here we want to implement CanActivate Interface, wich is define in @angular/router, 
+  so we are going to implement a method called canActivate(){}, wich should return true or false, 
+  so here we want to get the authentcation status of the current user, if the user is logged in we're going to 
+  return True, otherwise we're going to navigate them to the logging page and retrun false.
+  So in order to get the authentification state we need to inject our AuthService.
+  We need also navigate non authentified user to login page, so for that we need to inject the router service.
+  **/
+  canActivate(route, state: RouterStateSnapshot) {
+    // here with this map method, we're mapping an obsevable of firebase user to an observable of boolean.
     return this.auth.$user.map(user => {
       if (user) return true;
 
-      this.router.navigate(['/login']);
+      /* here we wanna pass a query parameter that determines the retrun Url, but how do we get this retrun url?
+
+      **/
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
-    })
+    });
   }
 
 }
